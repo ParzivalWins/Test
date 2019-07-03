30bd5e87badeb5c83dd1cddcfe07cd38427232a4
Parzival <ParzivalWins@users.noreply.github.com>
2019-07-03 13:39:33 -0700

    RemoveHello-AddHelloWorldReact

--- hello.js
+++
@@ -1,9 +0,0 @@
-Working Copy } Recognizes }  Code types . 
-
-'use strict';
-
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/**/*.test.js'],
-  testPathIgnorePatterns: ['/src/', 'node_modules'],
-};
